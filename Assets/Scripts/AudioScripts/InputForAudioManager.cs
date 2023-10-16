2ce44079cf2046306817ea33bdb4bb8660dff665
tarikhh02 <tarik.hajhamdo@gmail.com>
2023-10-16 12:56:05 +0200

    Fixed sound bug

--- Assets/Scripts/AudioScripts/InputForAudioManager.cs
+++ Assets/Scripts/AudioScripts/InputForAudioManager.cs
@@ -1,4 +1,5 @@
-﻿using Assets.Scripts.MovementManager;
+﻿using Assets.Scripts.ClampManagerScripts;
+using Assets.Scripts.MovementManager;
 using Assets.Scripts.SpeedManager;
 using System;
 using UnityEngine;
@@ -7,13 +8,18 @@ namespace Race_game_project.AudioInputManagement
 {
     public class InputForAudioManager : MonoBehaviour, IInputForAudioManager
     {
+        [SerializeField]
+        bool _isPlayer = false;
         ISpeedManager _speedManager;
         IObjectMover _objectMover;
+        IClampingDeterminationManager _clampManager;
         bool _hasEngagedBraking = false;
         private void Awake()
         {
             _speedManager = this.GetComponent<SpeedManager>();
             _objectMover = this.GetComponent<ObjectMover>();
+            if(_isPlayer)
+                _clampManager = this.GetComponent<ClampingDeterminationManager>();
         }
         public void GetInputForAudio(ref Race_game_project.AudioScripts.State _nextState, AudioSource braking, AudioSource spinningWheels)
         {
@@ -30,7 +36,7 @@ namespace Race_game_project.AudioInputManagement
                 _hasEngagedBraking = false;
             if (speed == 0 && (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.S)))
                 spinningWheels.Play();
-            if (speed == 0 || _speedManager.GetIsLoweringSpeed())
+            if (speed == 0 || _speedManager.GetIsLoweringSpeed() || (_isPlayer && _clampManager.PlayerMovementNeedsToBeClamped()))
                 _nextState = Race_game_project.AudioScripts.State.Idle;
             else if (_speedManager.GetHasAchievedMaximumSpeed())
                 _nextState = Race_game_project.AudioScripts.State.MaxSpeed;
